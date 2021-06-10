2814cac336e97ceab4ffef646b13fc51446bb1a7
Choi KiYong <gychoi0916@gmail.com>
2021-06-10 22:13:47 +0900

    #1.1~2 GET MOVIES Query

---
+++ src/routes/Detail.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+    query getMovie($id: Int!) {
+        movie(id: $id) {
+            id
+            title
+            medium_cover_image
+            description_intro
+        }
+    }
+`;
+export default () => {
+    const { id } = useParams();
+    const { loading, data } = useQuery(GET_MOVIE, {
+        variables: {id:+id}
+    });
+    // console.log(loading,data);
+    // console.log(id);
+    if(loading) {
+        return "loading";
+    }
+    if(data && data.movie) {
+        return data.movie.title;
+    }
+    return "Detail";
+};
