f61affe228d7d469752f21141ca00414f021e3cb
Choi KiYong <gychoi0916@gmail.com>
2021-06-10 22:33:49 +0900

    #1.3 Add Style

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,29 +2,66 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/client";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
     query getMovie($id: Int!) {
         movie(id: $id) {
-            id
             title
             medium_cover_image
+            language
+            rating
             description_intro
         }
     }
 `;
+
+const Container = styled.div `
+    height: 100vh;
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: white;
+`;
+
+const Column = styled.h1 `
+    margin-left: 10px;
+`;
+
+const Title = styled.h1 `
+    font-size: 56px;
+    margin-bottom: 15px;
+`;
+const Subtitle = styled.h4 `
+    font-size: 35px;
+    margin-bottom: 10px;
+`;
+
+const Description = styled.p `
+    font-size: 28px;
+`;
+
+const Poster = styled.div `
+    width: 25%;
+    height: 60%;
+    background-color: transparent;
+`;
+
 export default () => {
     const { id } = useParams();
     const { loading, data } = useQuery(GET_MOVIE, {
         variables: {id:+id}
     });
-    // console.log(loading,data);
-    // console.log(id);
-    if(loading) {
-        return "loading";
-    }
-    if(data && data.movie) {
-        return data.movie.title;
-    }
-    return "Detail";
+    return (
+        <Container>
+            <Column>
+                <Title>Name</Title>
+                <Subtitle>English  4.5</Subtitle>
+                <Description>lorem ipsum lalalla</Description>
+            </Column>
+            <Poster></Poster>
+        </Container>
+    )
 };
